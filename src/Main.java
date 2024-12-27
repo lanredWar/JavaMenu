a0938e90c6bec7792b088cfa5a6d32e8b864d6e1
jonatest b <jonathanbernale@gmail.com>
2024-12-27 08:23:24 -0600

    Se implementa el metodo menu y se crea un array con los meses, tambien se imprimen los 3 primeros.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,7 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import static ui.UIMenu.showMenu;
+
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor();
-        myDoctor.name = "Jonathan";
-        myDoctor.showName();
-        myDoctor.showId();
-        System.out.println(Doctor.id);
-
-        Doctor myDoctorJonh = new Doctor();
-        myDoctor.showId();
-        System.out.println(Doctor.id);
+        showMenu();
     }
 }
